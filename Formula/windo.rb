b18f610c9f31388fe7505212455e01d2c1926a86
dannelliu <liu.fanglin@windoent.com>
2026-05-10 18:11:06 +0800

    no message

--- Formula/windo.rb
+++ Formula/windo.rb
@@ -1,24 +1,33 @@
 class Windo < Formula
   desc "A CLI tool for generating kratos project templates"
-  homepage "https://git.code.tencent.com/windo-/homebrew-windo"
+  homepage "https://github.com/windoent/homebrew-windo"
 
   version "0.0.1"
 
   on_macos do
     on_intel do
-      url "https://git.code.tencent.com/windo-/homebrew-windo/uploads/2/tags/untagged-FFB5ADF1FDB14C09B7BE4B513B9646BC/windo-darwin-amd64"
-      sha256 "14381d1aaa3fbc46ad624a255a021f0c1610c9de0f8c26c7e4202abe1dd6cad1"
+      url "https://github.com/windoent/homebrew-windo/releases/download/v0.0.1/windo-darwin-amd64"
+      sha256 "ac9986f44fd699e095652ecd6674d2db81038e97d08d6d023c9eb4d3f046283a"
     end
 
     on_arm do
-      url "https://git.code.tencent.com/windo-/homebrew-windo/uploads/2/tags/untagged-DEFD94CBAF4541EBAFAD1F62A2AEBC5A/windo-darwin-arm64"
-      sha256 "691d0ce95c4ccd5a5fa6760fc5bab39b517886515a19a51636f53aaac2cd9cbf"
+      url "https://github.com/windoent/homebrew-windo/releases/download/v0.0.1/windo-darwin-arm64"
+      sha256 "000727afd77132af9e120e5a8eb84151a15db945140ff593d7dc0d102ccbed71"
     end
   end
 
   on_linux do
-    url "https://git.code.tencent.com/windo-/homebrew-windo/uploads/2/tags/untagged-E9F92923BFAB47528CEE884A795CEF4A/windo-linux-amd64"
-    sha256 "555cfd52a27eba327fa6223653295de4b0c6288248cbabf58f93bc201d6ce5ba"
+    url "https://github.com/windoent/homebrew-windo/releases/download/v0.0.1/windo-linux-amd64"
+    sha256 "30271cce73115e51c2101971ad4b404c699b3fd9d277dfc11f186b8d85d3e8d2"
+  end
+
+  on_macos do
+    on_intel do
+      resource "windo-windows" do
+        url "https://github.com/windoent/homebrew-windo/releases/download/v0.0.1/windo-windows.exe"
+        sha256 "28c83dc55aacc7d5541606933b0b59a6806203102b2514a1d68115fbd5052e97"
+      end
+    end
   end
 
   def install
